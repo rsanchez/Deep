8e84f50a1b4c4b0b5a3d5019227c6913945a210a
Rob Sanchez <rob@robsanchez.com>
2014-01-01 12:25:32 -0600

    fix bootstrap.php

--- src/rsanchez/Entries/bootstrap.php
+++ src/rsanchez/Entries/bootstrap.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once __DIR__.'/rsanchez/Entries/DbInterface.php';
-require_once __DIR__.'/rsanchez/Entries/Db.php';
-require_once __DIR__.'/rsanchez/Entries/Channel.php';
-require_once __DIR__.'/rsanchez/Entries/Channels.php';
-require_once __DIR__.'/rsanchez/Entries/Factory.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Field.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Query.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Entries/Entry.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Entries/Collection.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Entries/Filter.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Entries/Query.php';
-require_once __DIR__.'/rsanchez/Entries/Channel/Entries.php';
+require_once __DIR__.'/DbInterface.php';
+require_once __DIR__.'/Db.php';
+require_once __DIR__.'/Channel.php';
+require_once __DIR__.'/Channels.php';
+require_once __DIR__.'/Factory.php';
+require_once __DIR__.'/Channel/Field.php';
+require_once __DIR__.'/Channel/Query.php';
+require_once __DIR__.'/Channel/Entries/Entry.php';
+require_once __DIR__.'/Channel/Entries/Collection.php';
+require_once __DIR__.'/Channel/Entries/Filter.php';
+require_once __DIR__.'/Channel/Entries/Query.php';
+require_once __DIR__.'/Channel/Entries.php';
