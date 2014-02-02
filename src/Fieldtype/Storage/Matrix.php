ab92e6fe251c1baf0054c3dc1f3c09f98ad516c8
Rob Sanchez <rob@robsanchez.com>
2014-02-02 16:34:07 -0600

    Use local DB in Storage\Matrix

--- src/Fieldtype/Storage/Matrix.php
+++ src/Fieldtype/Storage/Matrix.php
@@ -13,14 +13,14 @@ class Matrix
 
     public function __invoke(array $entryIds, array $fieldIds)
     {
-        $query = ee()->db->where_in('field_id', $fieldIds)
+        $query = $this->db->where_in('field_id', $fieldIds)
                     ->get('matrix_cols');
 
         $cols = $query->result();
 
         $query->free_result();
 
-        $query = ee()->db->where_in('field_id', $fieldIds)
+        $query = $this->db->where_in('field_id', $fieldIds)
                     ->where_in('entry_id', $entryIds)
                     ->order_by('entry_id asc, field_id asc, row_order asc')
                     ->get('matrix_data');
