2d431dc2d34c73a80224b2c2836f60a558112370
Rob Sanchez <rob@robsanchez.com>
2014-02-02 08:39:15 -0600

    Add base Fieldtype\Collection class

---
+++ src/Fieldtype/Collection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace rsanchez\Deep\Fieldtype;
+
+use rsanchez\Deep\Fieldtype\Fieldtype;
+use IteratorAggregate;
+
+class Collection implements IteratorAggregate
+{
+    protected $fieldtypes = array();
+    protected $fieldtypesByName = array();
+
+    public function push(Fieldtype $fieldtype)
+    {
+        array_push($this->fieldtypes, $fieldtype);
+        $this->fieldtypesByName[$fieldtype->name] =& $fieldtype;
+    }
+
+    public function find($name)
+    {
+        //@TODO custom exception
+        if (! array_key_exists($name, $this->fieldtypesByName)) {
+            throw new \Exception('invalid fieldtype name');
+        }
+
+        return $this->fieldtypesByName[$name];
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->fieldtype);
+    }
+}
--- src/Fieldtype/Repository.php
+++ src/Fieldtype/Repository.php
@@ -5,13 +5,10 @@ namespace rsanchez\Deep\Fieldtype;
 use rsanchez\Deep\Fieldtype\Fieldtype;
 use rsanchez\Deep\Fieldtype\Storage;
 use rsanchez\Deep\Fieldtype\Factory;
-use IteratorAggregate;
+use rsanchez\Deep\Fieldtype\Collection;
 
-class Repository implements IteratorAggregate
+class Repository extends Collection
 {
-    private $fieldtypes = array();
-    private $fieldtypesByName = array();
-
     public function __construct(Storage $storage, Factory $factory)
     {
         foreach ($storage() as $row) {
@@ -20,26 +17,5 @@ class Repository implements IteratorAggregate
 
             $this->push($fieldtype);
         }
-    }
-
-    public function push(Fieldtype $fieldtype)
-    {
-        array_push($this->fieldtypes, $fieldtype);
-        $this->fieldtypesByName[$fieldtype->name] =& $fieldtype;
-    }
-
-    public function find($name)
-    {
-        //@TODO custom exception
-        if (! array_key_exists($name, $this->fieldtypesByName)) {
-            throw new \Exception('invalid fieldtype name');
-        }
-
-        return $this->fieldtypesByName[$name];
-    }
-
-    public function getIterator()
-    {
-        return new ArrayIterator($this->fieldtype);
     }
 }
