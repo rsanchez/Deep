95aa277c013e54d62802d881b9f03cf162927668
Rob Sanchez <rob@robsanchez.com>
2014-02-14 19:57:06 -0600

    Add row_id and other cols to Row class

--- src/Row/Row.php
+++ src/Row/Row.php
@@ -6,6 +6,13 @@ use rsanchez\Deep\Entity\Entity;
 
 class Row extends Entity
 {
+    public $row_id;
+    public $site_id;
+    public $entry_id;
+    public $field_id;
+    public $var_id;
+    public $row_order;
+
     public function id()
     {
         return $this->row_id;
