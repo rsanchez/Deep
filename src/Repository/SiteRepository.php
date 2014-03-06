291ab5646230dd31cae5e3a2ca625d9eba4af9ab
Rob Sanchez <rob@robsanchez.com>
2014-03-06 08:17:24 -0600

    Add SiteRepository

---
+++ src/Repository/SiteRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Deep
+ *
+ * @package      rsanchez\Deep
+ * @author       Rob Sanchez <info@robsanchez.com>
+ */
+
+namespace rsanchez\Deep\Repository;
+
+use rsanchez\Deep\Collection\SiteCollection;
+
+/**
+ * Repository of all Sites
+ */
+class SitesRepository
+{
+    /**
+     * Collection of all Channels
+     * @var \rsanchez\Deep\Collection\SiteCollection
+     */
+    protected $collection;
+
+    /**
+     * Constructor
+     *
+     * @param \rsanchez\Deep\Collection\SiteCollection $collection
+     */
+    public function __construct(SiteCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * Get Collection of all Sites
+     *
+     * @return \rsanchez\Deep\Collection\SiteCollection
+     */
+    public function getSites()
+    {
+        return $this->collection;
+    }
+}
