84c4a76b4f4bbde7d79b50b1ffbb536c21872a93
Rob Sanchez <rob@robsanchez.com>
2014-05-31 12:20:01 -0500

    Remove some cruft

--- tests/FieldRepositoryTest.php
+++ tests/FieldRepositoryTest.php
@@ -90,7 +90,4 @@ class FieldRepositoryTest extends PHPUnit_Framework_TestCase
 
         $this->assertThat($ids, new ArrayHasOnlyValuesConstraint([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]));
     }
-
-
-    //;
 }
