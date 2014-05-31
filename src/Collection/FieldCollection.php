24f7c2c3df44828aef0011c7bbbe25e024aab4ae
Rob Sanchez <rob@robsanchez.com>
2014-05-31 12:19:39 -0500

    Refactoring

--- src/Collection/FieldCollection.php
+++ src/Collection/FieldCollection.php
@@ -16,4 +16,85 @@ use rsanchez\Deep\Collection\AbstractFieldCollection;
  */
 class FieldCollection extends AbstractFieldCollection
 {
+    /**
+     * Fieldtypes used by this collection
+     * @var array
+     */
+    protected $fieldtypes = array();
+
+    /**
+     * Map of fieldtypes to field IDs:
+     *    'fieldtype_name' => array(1, 2, 3),
+     * @var array
+     */
+    protected $fieldIdsByFieldtype = array();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $fields = array())
+    {
+        parent::__construct($fields);
+
+        foreach ($fields as $field) {
+            $this->addFieldtype($field->field_type);
+
+            $this->fieldIdsByFieldtype[$field->field_type][] = $field->field_id;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function push($field)
+    {
+        $this->addFieldtype($field->field_type);
+
+        $this->fieldIdsByFieldtype[$field->field_type][] = $field->field_id;
+
+        return parent::push($field);
+    }
+
+    /**
+     * Check if this collection uses the specified fieldtype
+     *
+     * @param  string  $fieldtype
+     * @return boolean
+     */
+    public function hasFieldtype($fieldtype)
+    {
+        return in_array($fieldtype, $this->fieldtypes);
+    }
+
+    /**
+     * Get the field IDs for the specified fieldtype
+     *
+     * @param  string $fieldtype
+     * @return array
+     */
+    public function getFieldIdsByFieldtype($fieldtype)
+    {
+        return isset($this->fieldIdsByFieldtype[$fieldtype]) ? $this->fieldIdsByFieldtype[$fieldtype] : array();
+    }
+
+    /**
+     * Get a list of names of fieldtypes in this collection
+     * @return array
+     */
+    public function getFieldtypes()
+    {
+        return $this->fieldtypes;
+    }
+
+    /**
+     * Add a fieldtype to the list of fieldtypes in this collection
+     * @param  string $fieldtype
+     * @return void
+     */
+    public function addFieldtype($fieldtype)
+    {
+        if (! in_array($fieldtype, $this->fieldtypes)) {
+            $this->fieldtypes[] = $fieldtype;
+        }
+    }
 }
