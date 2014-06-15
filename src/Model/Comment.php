d90f1ea3c5b3e9cc9dbed19de9f2c5c0380dd0a5
Rob Sanchez <rob@robsanchez.com>
2014-06-14 20:13:02 -0500

    Add date accessors to Comment model

--- src/Model/Comment.php
+++ src/Model/Comment.php
@@ -38,4 +38,26 @@ class Comment extends Model
     {
         return $this->hasOne('\\rsanchez\\Deep\\Model\\Member', 'member_id', 'author_id');
     }
+
+    /**
+     * Get the comment_date column as a Carbon object
+     *
+     * @param  int            $value unix time
+     * @return \Carbon\Carbon
+     */
+    public function getCommentDateAttribute($value)
+    {
+        return Carbon::createFromFormat('U', $value);
+    }
+
+    /**
+     * Get the edit_date column as a Carbon object
+     *
+     * @param  int            $value unix time
+     * @return \Carbon\Carbon|null
+     */
+    public function getEditDateAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('U', $value) : null;
+    }
 }
