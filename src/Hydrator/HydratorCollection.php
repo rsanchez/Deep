ffb1a4f5d050b29146aff92c68dd8141c7d6b424
Rob Sanchez <rob@robsanchez.com>
2014-12-04 20:59:12 -0600

    Fix HydratorCollection put method

--- src/Hydrator/HydratorCollection.php
+++ src/Hydrator/HydratorCollection.php
@@ -4,15 +4,32 @@ namespace rsanchez\Deep\Hydrator;
 
 use Illuminate\Support\Collection;
 
+/**
+ * Collection of Hydrators
+ */
 class HydratorCollection extends Collection
 {
+    /**
+     * Throw an exception if attempting to push a Hydrator without specifying type
+     */
     public function push($value)
     {
-        throw new \Exception('Hydrators must have a key. Use ->put instead.');
+        throw new \Exception('Hydrators must have a key. Use put method instead.');
     }
 
-    public function put($type, AbstractHydrator $hydrator)
+    /**
+     * Add a Hydrator to the collection
+     *
+     * @param  string                                   $type
+     * @param  \rsanchez\Deep\Hydrator\AbstractHydrator $hydrator
+     * @return void
+     */
+    public function put($type, $hydrator)
     {
+        if (! $hydrator instanceof AbstractHydrator) {
+            throw new \Exception('$hydrator must be instance of \rsanchez\Deep\Hydrator\AbstractHydrator');
+        }
+
         return parent::put($type, $hydrator);
     }
 }
