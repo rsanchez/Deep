071b9c1fc9d5dcc92e3d3ea162306f2d22292bfd
Rob Sanchez <rob@robsanchez.com>
2015-01-03 11:03:10 -0600

    Fix GridHydrator not assigning rows properly

---
+++ src/Model/NullProperty.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Deep
+ *
+ * @package      rsanchez\Deep
+ * @author       Rob Sanchez <info@robsanchez.com>
+ */
+
+namespace rsanchez\Deep\Model;
+
+/**
+ * A property that does nothing
+ */
+class NullProperty extends AbstractProperty
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrefix()
+    {
+        return '';
+    }
+}
