5df35bfe2cf05990a1e91a1d4036f5e0c6f614a4
Rob Sanchez <rob@robsanchez.com>
2015-02-06 07:56:26 -0600

    Finish FileHydrator setter

--- src/Hydrator/FileHydrator.php
+++ src/Hydrator/FileHydrator.php
@@ -101,11 +101,36 @@ class FileHydrator extends AbstractHydrator
 
     /**
      * Setter callback
-     * @param  \rsanchez\Deep\Model\File|null $value
+     * @param  \rsanchez\Deep\Model\File|string|int|null $value
      * @return \rsanchez\Deep\Model\File|null
      */
-    public function setter(File $value = null)
+    public function setter($value = null, PropertyInterface $property = null)
     {
-        return $value;
+        if (is_null($value)) {
+            return null;
+        }
+
+        if ($value instanceof File) {
+            return $value;
+        }
+
+        if (is_int($value)) {
+            return File::find($value);
+        }
+
+        if (is_string($value)) {
+            // is it an ID?
+            if (preg_match('/^\d+$/', $value)) {
+                return File::find($value);
+            }
+
+            if (preg_match('/^{filedir_(\d+)}(.*?)$/', $value, $match)) {
+                return File::uploadPrefId($match[1])
+                    ->fileName($match[2])
+                    ->first();
+            }
+        }
+
+        throw new \InvalidArgumentException('$value must be of type string, int, null, or \rsanchez\Deep\Model\File.');
     }
 }
