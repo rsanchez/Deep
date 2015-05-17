058034649261ec49312680e725579da11030db89
Rob Sanchez <rob@robsanchez.com>
2015-05-17 10:51:04 -0500

    Add Asset save test

---
+++ tests/functional/AbstractModelSaveTest.php
@@ -0,0 +1,101 @@
+<?php
+
+use rsanchez\Deep\Model\Model;
+use Symfony\Component\Translation\Translator;
+use rsanchez\Deep\Validation\Factory as ValidatorFactory;
+use rsanchez\Deep\Validation\DatabasePresenceVerifier;
+
+abstract class AbstractModelSaveTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $validationTranslator = new Translator('en');
+
+        $validatorFactory = new ValidatorFactory($validationTranslator);
+
+        $validationPresenceVerifier =  new DatabasePresenceVerifier(Model::getConnectionResolver());
+
+        $validatorFactory->setPresenceVerifier($validationPresenceVerifier);
+
+        Model::setValidatorFactory($validatorFactory);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        Model::unsetValidatorFactory();
+    }
+
+    /**
+     * Get the class name of the model being tested
+     * @return string
+     */
+    abstract protected function getModelClass();
+
+    /**
+     * Get the default attributes when creating a new model instance
+     * @return array  attr => value
+     */
+    abstract protected function getModelAttributes();
+
+    /**
+     * Create a new model instance
+     * @return \rsanchez\Deep\Model\Model
+     */
+    protected function createModel()
+    {
+        $class = $this->getModelClass();
+
+        $attributes = $this->getModelAttributes();
+
+        return new $class($attributes);
+    }
+
+    /**
+     * Test the model save method
+     * @return void
+     */
+    public function testSave()
+    {
+        $class = $this->getModelClass();
+
+        $model = $this->createModel();
+
+        $model->save();
+
+        $id = $model->getKey();
+
+        // if the save was successful, you should be able to find new record in DB
+        $model = call_user_func([$class, 'find'], $id);
+
+        $this->assertInstanceOf($class, $model);
+    }
+
+    /**
+     * Test model validation by supplying model attributes that will trigger validation
+     * errors with the specified messages.
+     *
+     * @param  array        $attributes key/value pairs of attributes to set on the model
+     * @param  string|array $messages one or more validation messages expected
+     * @return void
+     */
+    protected function validateExceptionTest($attributes, $messages)
+    {
+        $message = implode(PHP_EOL, (array) $messages);
+
+        $this->setExpectedException('rsanchez\Deep\Exception\ValidationException', $message);
+
+        $model = $this->createModel();
+
+        foreach ($attributes as $attribute => $value) {
+            $model->$attribute = $value;
+        }
+
+        $model->validateOrFail();
+    }
+}
---
+++ tests/functional/AssetModelSaveTest.php
@@ -0,0 +1,118 @@
+<?php
+
+use rsanchez\Deep\Model\Asset;
+use rsanchez\Deep\Model\UploadPref;
+use rsanchez\Deep\Repository\UploadPrefRepository;
+
+class AssetModelSaveTest extends AbstractModelSaveTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModelClass()
+    {
+        return 'rsanchez\Deep\Model\Asset';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModelAttributes()
+    {
+        return [
+            'folder_id' => 1,
+            'source_type' => 'ee',
+            'filedir_id' => 1,
+            'file_name' => 'foo.jpg',
+            'kind' => 'image',
+            'width' => 300,
+            'height' => 300,
+            'size' => 51200,
+            'search_keywords' => 'foo.jpg',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createModel()
+    {
+        $model = parent::createModel();
+
+        $uploadPrefRepository = new UploadPrefRepository(new UploadPref());
+
+        $model->setUploadPref($uploadPrefRepository->find($model->filedir_id));
+
+		return $model;
+    }
+
+    public function testFolderIdRequiredValidation()
+    {
+        $this->validateExceptionTest(['folder_id' => ''], 'The folder id field is required.');
+    }
+
+    public function testFolderIdExistsValidation()
+    {
+        $this->validateExceptionTest(['folder_id' => '10'], 'The selected folder id is invalid.');
+    }
+
+    public function testSourceTypeRequiredValidation()
+    {
+        $this->validateExceptionTest(['source_type' => ''], 'The source type field is required.');
+    }
+
+    public function testSourceIdRequiredValidation()
+    {
+        $this->validateExceptionTest(['source_type' => 's3', 'source_id' => ''], 'The source id field is required when source type is s3.');
+    }
+
+    public function testSourceIdExistsValidation()
+    {
+        $this->validateExceptionTest(['source_id' => '10'], 'The selected source id is invalid.');
+    }
+
+    public function testFiledirIdRequiredValidation()
+    {
+        $this->validateExceptionTest(['filedir_id' => '', 'source_type' => 'ee'], 'The filedir id field is required when source type is ee.');
+    }
+
+    public function testFiledirIdExistsValidation()
+    {
+        $this->validateExceptionTest(['filedir_id' => '10'], 'The selected filedir id is invalid.');
+    }
+
+    public function testFileNameRequiredValidation()
+    {
+        $this->validateExceptionTest(['file_name' => ''], 'The file name field is required.');
+    }
+
+    public function testKindRequiredValidation()
+    {
+        $this->validateExceptionTest(['kind' => ''], 'The kind field is required.');
+    }
+
+    public function testKindInValidation()
+    {
+        $this->validateExceptionTest(['kind' => 'foo'], 'The selected kind is invalid.');
+    }
+
+    public function testWidthIntegerValidation()
+    {
+        $this->validateExceptionTest(['width' => 'foo'], 'The width must be an integer.');
+    }
+
+    public function testHeightIntegerValidation()
+    {
+        $this->validateExceptionTest(['height' => 'foo'], 'The height must be an integer.');
+    }
+
+    public function testSizeRequiredValidation()
+    {
+        $this->validateExceptionTest(['size' => ''], 'The size field is required.');
+    }
+
+    public function testSizeIntegerValidation()
+    {
+        $this->validateExceptionTest(['size' => 'foo'], 'The size must be an integer.');
+    }
+}
